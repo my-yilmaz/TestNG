ce7999b7a19763fe430caa763ca325aead2c0259
my-yilmaz <myyilmaz046@gmail.com>
2022-05-30 17:25:47 +0300

    30.05.2022/17:25

--- src/test/java/tests/day17_pom/C02_PageClassKullanimi.java
+++ src/test/java/tests/day17_pom/C02_PageClassKullanimi.java
@@ -7,15 +7,16 @@ import pages.AmazonPage;
 import utilities.Driver;
 
 public class C02_PageClassKullanimi {
-    AmazonPage amazonPage=new AmazonPage();
     @Test
     public void test01() {
-
+        AmazonPage amazonPage = new AmazonPage();
+        // amazona gidelim
         Driver.getDriver().get("https://www.amazon.com");
-        amazonPage.aramaKutusu.sendKeys("nutella"+ Keys.ENTER);
-        String actualSonuc=amazonPage.aramaSonucElementi.getText();
-        String expectedSonuc="nutella";
-        Assert.assertTrue(actualSonuc.contains(expectedSonuc));
-        Driver.closeDriver();
+        // nutella aratalim
+        amazonPage.aramaKutusu.sendKeys("nutella" + Keys.ENTER);
+        // sonuc yazisinin nutella icerdigini test edelim
+        String actualSonuc = amazonPage.aramaSonucElementi.getText();
+        String arananKelime = "nutella";
+        Assert.assertTrue(actualSonuc.contains(arananKelime));
     }
 }
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -2,6 +2,10 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import java.time.Duration;
 public class Driver {
     /*
@@ -10,11 +14,34 @@ public class Driver {
     driver olusturup, ilgili ayarlarin yapilmasi
     ve en sonda driver'in kapatilmasi tercih edilmistir.
      */
-   static WebDriver driver;
+    static WebDriver driver;
     public static WebDriver getDriver(){
         if (driver==null) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            switch (ConfigReader.getProperty("browser")){
+                case "chrome" :
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "edge" :
+                    WebDriverManager.edgedriver().setup();
+                    driver=new EdgeDriver();
+                    break;
+                case "firefox" :
+                    WebDriverManager.firefoxdriver().setup();
+                    driver=new FirefoxDriver();
+                    break;
+                case "headless-chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
+                case "headless-edge": //
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver(new EdgeOptions().setHeadless(true));
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+            }
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
